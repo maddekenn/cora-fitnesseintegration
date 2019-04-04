1ec35c72666df57a89d7fb57190e8cd48d10107c
olovm <olov.mckie@ub.uu.se>
2019-04-04 13:06:45 +0000

    updating for use of java modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module se.uu.ub.cora.fitnesseintegration {
+	requires transitive se.uu.ub.cora.clientdata;
+	requires transitive se.uu.ub.cora.httphandler;
+	requires transitive java.ws.rs;
+	requires java.xml;
+
+	exports se.uu.ub.cora.fitnesseintegration;
+}
